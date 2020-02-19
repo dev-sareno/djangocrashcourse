4f91724b5a645ea0a023b75e70c7634856b12e7a
Jay-ar Sareno <eladio.sareno@netzon.com.se>
2020-02-19 14:03:47 +0800

    File upload: return the content of file

--- uploads/views.py
+++ uploads/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse, HttpRequest, Http404, JsonResponse, HttpRe
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
+import os
+from django.conf import settings
+from django.core.files.storage import default_storage
 
 
 class UploadResponse:
@@ -29,13 +32,24 @@ def upload_json(request: HttpRequest):
         if 'file' not in request.FILES:
             return HttpResponse('not found: file', status=400)
 
-        file = request.FILES['file']
+        file_param = request.FILES['file']
         fs = FileSystemStorage()
-        filename = fs.save(file.name, file)
+        filename = fs.save(file_param.name, file_param)
         uploaded_file_url = fs.url(filename)
+
+        # file_path = os.path.join(settings.BASE_DIR, uploaded_file_url)
+        file_path = "%s%s" % (settings.BASE_DIR, uploaded_file_url)
+        file = default_storage.open(file_path)
+        data = file.read()  # bytes
+        file.close()
+
+        json_string = data.decode("utf-8")
+        json_ob = json.loads(json_string)
 
         response = UploadResponse(1, 'Successful')
         response['path'] = uploaded_file_url
+        response['path_sys'] = file_path
+        response['data'] = json_ob
 
         return JsonResponse(response.to_json(), status=200)
     else:
